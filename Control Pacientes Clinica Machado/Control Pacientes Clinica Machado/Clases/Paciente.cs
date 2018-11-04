ade0202d746a49a39b9d0f19462c15c49e9a0fea
PENA98 <maynorx5000@gmail.com>
2018-11-04 00:50:14 -0600

    Creacion de las clases

---
+++ Control Pacientes Clinica Machado/Control Pacientes Clinica Machado/Clases/Paciente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Pacientes_Clinica_Machado.Clases
+{
+    class Paciente
+    {
+
+    }
+}
